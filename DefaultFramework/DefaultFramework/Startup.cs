a1a42f25e185cfc747bbbf8838ae44ab5a4c6339
reza.septiandra <reza.septiandra@SML-NETDEV002>
2017-08-29 09:56:31 +0700

    as

---
+++ DefaultFramework/DefaultFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DefaultFramework.Startup))]
+namespace DefaultFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
